4fcd491e90a069e610e146c5f6905bb6692847da
Guy Pursey <guypursey@gmail.com>
2014-11-02 16:17:03 +0000

    Added script for writing Scriptogram metadata files.
    It takes the title from the level 1 header at the top of the post.
    It takes the current date.
    It adds a "Published: True" line.
    
    It does not yet do anything about tags.

---
+++ create_metascrp.sh
@@ -0,0 +1,5 @@
+header=$(grep -E --max-count=1 '^\#[^#]*?$' $1.md)
+bareheader=${header#\# }
+contents=$(grep -Ev '^\#[^#]*?$' $1.md)
+metadata=$(echo -e "Title: $bareheader\nDate: $(date '+%F %H:%M')\nPublished: True\n")
+echo -e "$metadata" > test.txt
