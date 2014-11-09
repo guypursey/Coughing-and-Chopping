5727dd603c9203e73bccf8485095d21e5901e385
Guy Pursey <guypursey@gmail.com>
2014-11-09 16:23:27 +0000

    Corrected file for loading metascrp data to.

--- create_metascrp.sh
+++ create_metascrp.sh
@@ -2,4 +2,4 @@ header=$(grep -E --max-count=1 '^\#[^#]*?$' $1.md)
 bareheader=${header#\# }
 contents=$(grep -Ev '^\#[^#]*?$' $1.md)
 metadata=$(echo -e "Title: $bareheader\nDate: $(date '+%F %H:%M')\nPublished: True\n")
-echo -e "$metadata" > test.txt
+echo -e "$metadata" > metascrp.txt
