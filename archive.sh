1e29072bdf9cd2c8ae58e6c1c550c856a5fa154a
Guy Pursey <guypursey@gmail.com>
2014-11-12 20:36:49 +0000

    Added new archive script to automate renaming and moving.

---
+++ archive.sh
@@ -0,0 +1,18 @@
+# Clean up file name.
+
+    cleanfilename=$(echo $1 | tr '_ ' \-)
+    newdirname=$(echo $(date '+%FT%H-%M')_$cleanfilename)
+
+## Move whole folder from `drafts` to `archives`, with new name.
+
+    mv ./drafts/$1 ./archives/$newdirname
+
+## Rename main file via Git command.
+
+	cd ./archives/$newdirname
+	git mv $1.md $newdirname.md
+	git commit -m "Renamed main file on publish."
+
+## Make a copy of the post in `posted` folder.
+
+    cp $newdirname.md ../../posted
