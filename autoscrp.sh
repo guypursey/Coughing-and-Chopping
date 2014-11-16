eb78edfc192f1e1c1fc701b35d65698215b2761d
Guy Pursey <guypursey@gmail.com>
2014-11-16 18:59:27 +0000

    Made `autoscrp.sh` independent of `create_metascrp.sh`.

--- autoscrp.sh
+++ autoscrp.sh
@@ -28,7 +28,7 @@ else
 	contents=$(grep -Ev '^\#[^#]*?$' $1.md)
 	tags="$(awk -v OFS=', ' -v RS= '{$1=$1}1' tags.txt)"
 	metadata=$(echo -e "Title: $bareheader\nDate: $(date '+%F %H:%M')\nPublished: True\nTags: $tags \n")
-	filedata=$(<metascrp.txt)$'\n\n'"$contents"
+	filedata="$metadata"$'\n\n'"$contents"
 	echo $1
 fi
 
