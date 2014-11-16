85b1b07936b43f8e4182dbb9d01df661a44bd461
Guy Pursey <guypursey@gmail.com>
2014-11-16 20:10:03 +0000

    Added new `publish.sh` file.
    New file executes `archive.sh` first then `autoscrp.sh`.

---
+++ publish.sh
@@ -0,0 +1,7 @@
+## Make folder definitive by moving out of `drafts` and into `archive`.
+
+	pieceref=$(./archive.sh $1)
+
+## Use reference for relevant piece to push out to Scriptogram.
+
+	./autoscrp.sh $pieceref
