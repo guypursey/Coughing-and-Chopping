46f697f140e1ffa0f2d14eb88cf266de08f1b05c
Guy Pursey <guypursey@gmail.com>
2014-11-16 18:38:47 +0000

    Refactored line in script for creating Scriptogram metadata.
    Got rid of new `tagsraw` variable from last commit by
    replacing couple of lines with code with a line from
    [a StackOverflow answer](http://stackoverflow.com/a/26831072/1563507).

--- create_metascrp.sh
+++ create_metascrp.sh
@@ -1,7 +1,6 @@
 header=$(grep -E --max-count=1 '^\#[^#]*?$' $1.md)
 bareheader=${header#\# }
 contents=$(grep -Ev '^\#[^#]*?$' $1.md)
-tagsraw=$(<tags.txt)
-tags=${tagsraw//$'\n'/', '}
+tags="$(awk -v OFS=', ' -v RS= '{$1=$1}1' tags.txt)"
 metadata=$(echo -e "Title: $bareheader\nDate: $(date '+%F %H:%M')\nPublished: True\nTags: $tags \n")
 echo -e "$metadata" > metascrp.txt
