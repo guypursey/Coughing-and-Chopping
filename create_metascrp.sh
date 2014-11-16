db1e335da42797caeee38152dd30501f5fb35785
Guy Pursey <guypursey@gmail.com>
2014-11-16 19:01:36 +0000

    Removed now unnecessary `create_metascrp.sh`.

--- create_metascrp.sh
+++
@@ -1,6 +0,0 @@
-header=$(grep -E --max-count=1 '^\#[^#]*?$' $1.md)
-bareheader=${header#\# }
-contents=$(grep -Ev '^\#[^#]*?$' $1.md)
-tags="$(awk -v OFS=', ' -v RS= '{$1=$1}1' tags.txt)"
-metadata=$(echo -e "Title: $bareheader\nDate: $(date '+%F %H:%M')\nPublished: True\nTags: $tags \n")
-echo -e "$metadata" > metascrp.txt
