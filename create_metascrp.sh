4cd647f9c09c70918a3ba4de66bbf76eda521e7c
Guy Pursey <guypursey@gmail.com>
2014-11-16 18:30:44 +0000

    Update script that creates metadata file to handle tags.
    Tags can now sit in a plain-text file which features a one
    tag per line and `create_metascrp.sh` will take them all,
    put them on one comma-delimited line and put them into the
    `metascrp.txt` file that is then created for Scriptogram.

--- create_metascrp.sh
+++ create_metascrp.sh
@@ -1,5 +1,7 @@
 header=$(grep -E --max-count=1 '^\#[^#]*?$' $1.md)
 bareheader=${header#\# }
 contents=$(grep -Ev '^\#[^#]*?$' $1.md)
-metadata=$(echo -e "Title: $bareheader\nDate: $(date '+%F %H:%M')\nPublished: True\n")
+tagsraw=$(<tags.txt)
+tags=${tagsraw//$'\n'/', '}
+metadata=$(echo -e "Title: $bareheader\nDate: $(date '+%F %H:%M')\nPublished: True\nTags: $tags \n")
 echo -e "$metadata" > metascrp.txt
