becb5f29437ce841323661a66f0463358fe202ba
Guy Pursey <guypursey@gmail.com>
2015-02-15 10:13:14 +0000

    Added handling for fileslug and storage in `imagemap.csv`.
    A `$FILESLUG` variable takes care of the slug to be sent to
    Picasa and for subsequent recognition of the correct component
    in the returned XML content.

--- images/picasa/test-post-photo.sh
+++ images/picasa/test-post-photo.sh
@@ -17,7 +17,19 @@ fi
 FILESIZE=$(stat -c "%s" "../mongolian-flower.jpg")
 echo $FILESIZE
 
+FILESLUG="mongolian-flower-1.jpg"
+# How can we add slugs and files programmatically?
+# What if slug is already taken?
+
 # Try posting a photo with test xml.
-NEWIMG=$(curl https://picasaweb.google.com/data/feed/api/user/default/albumid/$ALBMID -X POST -H "GData-Version: 2" -H "Authorization: Bearer $ACCTOK" -H "Content-Type: image/jpeg" -H "Content-Length: $FILESIZE" -H "Slug: test-photo.jpg" --data-binary @../mongolian-flower.jpg)
+NEWIMG=$(curl https://picasaweb.google.com/data/feed/api/user/default/albumid/$ALBMID -X POST -H "GData-Version: 2" -H "Authorization: Bearer $ACCTOK" -H "Content-Type: image/jpeg" -H "Content-Length: $FILESIZE" -H "Slug: $FILESLUG" --data-binary @../mongolian-flower.jpg)
 
 echo $NEWIMG > _result_new_photo.xml
+
+re="<content type='.*' src='(.*/$FILESLUG)'/>"
+if [[ $NEWIMG =~ $re ]]; then
+	IMGURL=${BASH_REMATCH[1]}
+	echo $IMGURL
+fi
+
+echo "$FILESLUG, $IMGURL" >> ../imagemap.csv
