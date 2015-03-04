01147731e610ebd55cad1ed0ca3129bf71cbb9d2
Guy Pursey <guypursey@gmail.com>
2015-03-04 06:52:31 +0000

    Removed test files from Picasa folder.

--- images/picasa/test-post-with-access-token.sh
+++
@@ -1,7 +0,0 @@
-ACCTOK=$(<_access_token.txt)
-
-# Try posting with test xml.
-NEWALB=$(curl https://picasaweb.google.com/data/feed/api/user/default -X POST -H "GData-Version: 2" -H "Authorization: Bearer $ACCTOK" -H "Content-Type: application/atom+xml" -d @test-post-album.xml)
-echo $NEWALB > _result_new_album.xml
-
-# Above does actually work but album was invisible!
